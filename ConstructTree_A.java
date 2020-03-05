0eeb899e1d5e8e5d44a36a1887dc1560a541b2d8
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2020-03-04 22:29:39 -0500

    Update ConstructTree_A.java

--- ConstructTree_A.java
+++ ConstructTree_A.java
@@ -1,24 +1,24 @@
 import java.util.*;
-public class ConstructTree_C
+public class ConstructTree_A
 {
   public static void main(String[] args)
   {
     BST_rec BSTr = new BST_rec();
     AVL_Iter AVL = new AVL_Iter();
-    ArrayList<Integer> arr =getRandomA.getRandomArray(10000);
+    ArrayList<Integer> arr =getRandomA.getRandomArray(30000);
     long startTimeBSTr = System.currentTimeMillis();
     for(int i=0;i<arr.size();i++)
     {
-      BST.insertIter(i);
+      BSTr.insertrec(i);
     }
     long endTimeBSTr = System.currentTimeMillis();
     long timeBSTr =  ((endTimeBSTr-startTimeBSTr));
     System.out.println(timeBSTr + "ms");
 
     long startTimeAVL = System.currentTimeMillis();
-    for(int i=0;i<arr.size();i++)
+    for(int j=0;j<arr.size();j++)
     {
-      AVL.insertIter(i);
+      AVL.insertIter(j);
     }
     long endTimeAVL = System.currentTimeMillis();
     long timeAVL =  ((endTimeAVL- startTimeAVL));
