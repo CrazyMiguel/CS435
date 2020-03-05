ceb2a28aa02d188b870d4fe523d651947967c39d
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2020-03-04 21:28:46 -0500

    Add files via upload

---
+++ ConstructTree_A.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ConstructTree_C
+{
+  public static void main(String[] args)
+  {
+    BST_rec BSTr = new BST_rec();
+    AVL_Iter AVL = new AVL_Iter();
+    ArrayList<Integer> arr =getRandomA.getRandomArray(10000);
+    long startTimeBSTr = System.currentTimeMillis();
+    for(int i=0;i<arr.size();i++)
+    {
+      BST.insertIter(i);
+    }
+    long endTimeBSTr = System.currentTimeMillis();
+    long timeBSTr =  ((endTimeBSTr-startTimeBSTr));
+    System.out.println(timeBSTr + "ms");
+
+    long startTimeAVL = System.currentTimeMillis();
+    for(int i=0;i<arr.size();i++)
+    {
+      AVL.insertIter(i);
+    }
+    long endTimeAVL = System.currentTimeMillis();
+    long timeAVL =  ((endTimeAVL- startTimeAVL));
+    System.out.println(timeAVL + "ms");
+  }
+}
