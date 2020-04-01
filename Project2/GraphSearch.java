c46b3efd430ba92520a31118f2f3b2325be063bb
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2020-04-01 19:20:12 -0400

    Currently not recognizing type Node

--- Project2/GraphSearch.java
+++ Project2/GraphSearch.java
@@ -3,34 +3,49 @@ import java.util.*;
 class GraphSearch
 {
   //3d)
-  ArrayList<Node> DFSRec(final Node start, final Node end)
+  ArrayList<Node> DFSHelper(Node start, Node end, ArrayList<Node> arr)
   {
-    ArrayList<Node> DFS_order= new ArrayList<Node>;
     //The start always moves to the next node and becomes the new start
-    //until the order actually hit the end
+    //until the order array actually hit the end
+    //mark visited and add the start node to array if its not end
+    //now go to neighbors if not visited recurse
     //start deleting if you reached the end of the depth so you go up to the next node
-    start.visit-true;
-    if(!(DFS_order.contains(end)))
+    start.visit=true;
+    if(!(arr.contains(end)))
     {
-      DFS_order.add(start);
+      arr.add(start);
     }
-    for (neighbor : start.neighbors)
+    for (Node neighbor : start.neighbors)
     {
       //check if the neighbor node was visited false if not
       if(!(neighbor.visit))
       {
-        DFSRec(neighbor,end)//I need a helper function 
+        DFSRec(neighbor,end,arr);
       }
     }
+    if(!(arr.contains(end)))
+    {
+      if(arr.size()>0)
+      {
+        arr.remove(arr.size()-1);
+      }
+    }
+    return arr;
+  }
 
+  ArrayList<Node> DFSRec(final Node start, final Node end)
+  {
+    ArrayList<Node> DFS_order= new ArrayList<Node>();
+    DFSHelper(start,end,DFS_order);
 
-
-
-    //Base Case of nothing
+    //Base Case of nothing in the arraylist after dfs meaning no path
     if(DFS_order.size()==0)
     {
       return null;
     }
-
+    else
+    {
+      return DFS_order;
+    }
   }
 }
