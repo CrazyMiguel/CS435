7e07d8f2fd6f2329f424701e581545b3f08b32cb
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2020-04-05 16:39:15 -0400

    BFT recursive Done

--- Project2/GraphSearch.java
+++ Project2/GraphSearch.java
@@ -75,4 +75,53 @@ class GraphSearch
     }
     return null;
   }
+
+  //3f Breadth First Traversal
+  ArrayList<Node> BFTRec(final Graph graph)
+  {
+    ArrayList<Node> BFT_order= new ArrayList<Node>();
+    Queue<Node> queue = new LinkedList<>();
+    //Set a start node
+    queue.add(graph.vertices.get(0));
+    BFTHelper(queue, BFT_order, queue.peek());
+
+    //Base Case of nothing in the arraylist after dfs meaning no path
+    if(BFT_order.size()==0)
+    {
+      return null;
+    }
+    else
+    {
+      return BFT_order;
+    }
+  }
+
+  ArrayList<Node> BFTHelper(Queue<Node> queue, ArrayList<Node> arr, Node start)
+  {
+    //The start always moves to the next node and becomes the new start
+    //until the order array actually hit the end
+    //mark visited and add the start node to array if its not end
+    //now go to ***ALL*** the neighbors if not visited then recurse
+    //start deleting if you reached the end of the depth so you go up to the next node
+    if(!(queue.isEmpty()))
+    {
+      arr.add(start);
+      start.visit = true;
+      for (Node neighbor : start.neighbors)
+      {
+        //check if the neighbor node was visited false if not
+        if(!(neighbor.visit))
+        {
+          neighbor.visit = true;
+          queue.add(neighbor);
+        }
+      }
+      queue.poll();
+      if(!(queue.isEmpty()))
+      {
+        BFTHelper(queue,arr,queue.peek());
+      }
+    }
+    return arr;
+  }
 }
