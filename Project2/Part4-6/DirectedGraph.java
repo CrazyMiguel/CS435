a029d16218a61b8dace467be922932066fa8665d
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2020-04-07 14:10:57 -0400

    Just had to change to makes directions

---
+++ Project2/Part4-6/DirectedGraph.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class DirectedGraph
+{
+  ArrayList<Node> vertices=new ArrayList<>();
+
+  public void addNode(final int val)
+  {
+    Node node=new Node(val);
+    vertices.add(node);
+  }
+
+  public void addDirectedEdge(final Node first, final Node second)
+  {
+    //Directional might have to check if the nodes actually exist
+    if(!(first.neighbors.contains(second)))
+    {
+      first.neighbors.add(second);
+    }
+  }
+
+  public void removeDirectedEdge(final Node first, final Node second)
+  {
+    //Directional remove might have to check if the nodes actually exist
+    if(first.neighbors.contains(second))
+    {
+      first.neighbors.remove(second);
+      second.neighbors.remove(first);
+    }
+  }
+
+  public HashSet<Node> getAllNodes()
+  {
+    HashSet<Node> set = new HashSet<Node>(vertices);
+    return set;
+  }
+
+  public void print()
+  {
+    //checking if the neighbors and vertexes being added correctly depending on comment out
+    for(int i=0;i<vertices.size();i++)
+    {
+      Node curr=vertices.get(i);
+      for(int j=0;j<curr.neighbors.size();j++)
+      {
+        System.out.println("Vertex " + curr.data);
+        System.out.println(" -> " + curr.neighbors.get(j).data);
+      }
+      //System.out.println(curr.data);
+    }
+  }
+}
