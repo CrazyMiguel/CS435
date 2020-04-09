7b262553d1f50bcdad775b1fc9560e40789ef857
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2020-04-09 18:57:34 -0400

    createRandomGridGraph Done!! I hope

--- Project2/Part4-6/Question6/Main3.java
+++ Project2/Part4-6/Question6/Main3.java
@@ -2,19 +2,40 @@ import java.util.*;
 
 class Main3
 {
+  public static GridGraph createRandomGridGraph(int n)
+  {
+    GridGraph randG = new GridGraph();
+    Random r=new Random();
+
+    //Assigning nodes
+    int count=0;
+    for (int y=0;y<n;y++)
+    {
+      for (int x=0;x<n;x++)
+      {
+        randG.addGridNode(count,x,y);
+        count+=1;
+      }
+    }
+
+    //Assign random edges from low to high
+    for (int j=0; j<randG.vertices.size();j++)
+    {
+      for(int k=0; k<randG.vertices.size();k++)
+      {
+        int chance=r.nextInt(2);//50% chance of getting an edge
+        if(chance==0)
+        {
+          randG.addUndirectedEdge(randG.vertices.get(j), randG.vertices.get(k));
+        }
+      }
+    }
+    return randG;
+  }
 
   public static void main(String[] args)
   {
-    GridGraph graph=new GridGraph();
-    graph.addGridNode(0,0,0);
-    graph.addGridNode(1,0,1);
-    graph.addGridNode(2,1,0);
-    graph.addGridNode(3,0,2);
-    graph.addUndirectedEdge(graph.vertices.get(0),graph.vertices.get(1));
-    graph.addUndirectedEdge(graph.vertices.get(0),graph.vertices.get(2));
-    graph.addUndirectedEdge(graph.vertices.get(0),graph.vertices.get(3));
-    graph.print();
-    graph.removeUndirectedEdge(graph.vertices.get(0),graph.vertices.get(2));
-    //graph.print();
+    GridGraph randG = createRandomGridGraph(3);
+    randG.print();
   }
 }
