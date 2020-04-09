ed553324d904ee3ce812e516bf21ba25c8ebc084
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2020-04-09 13:00:40 -0400

    random function created 5c done

--- Project2/Part4-6/Question5/Main2.java
+++ Project2/Part4-6/Question5/Main2.java
@@ -2,9 +2,36 @@ import java.util.*;
 
 class Main2
 {
+  public static WeightedGraph createRandomCompleteWeightedGraph(final int n)
+  {
+    WeightedGraph randG = new WeightedGraph();
+    Random r=new Random();
+
+    //Assigning nodes
+    for (int i=0;i<n;i++)
+    {
+      randG.addNode(i);
+    }
+
+    //Assign random edges from low to high
+    for (int j=0; j<randG.vertices.size();j++)
+    {
+      for(int k=0; k<randG.vertices.size();k++)
+      {
+        int weight=r.nextInt(10)+1;
+        if (j!=k)
+        {
+        randG.addWeightedEdge(randG.vertices.get(j), randG.vertices.get(k), weight);
+        }
+      }
+    }
+    return randG;
+  }
   public static void main(String[] args)
   {
     WeightedGraph graph=new WeightedGraph();
+    WeightedGraph randG = createRandomCompleteWeightedGraph(5);
+    randG.print();
     graph.addNode(0);
     graph.addNode(1);
     graph.addNode(2);
@@ -12,6 +39,6 @@ class Main2
     graph.addWeightedEdge(graph.vertices.get(0),graph.vertices.get(1),1);
     graph.addWeightedEdge(graph.vertices.get(1),graph.vertices.get(2),1);
     graph.removeWeightedEdge(graph.vertices.get(1),graph.vertices.get(2));
-    graph.print();
+    //graph.print();
   }
 }
