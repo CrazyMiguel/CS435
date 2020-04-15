9ecd0a26f337d4d41278519111606b26a9c4c031
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2020-04-15 18:00:06 -0400

    Made arraylist to hashset based on comments

--- Project2/Part4-6/Question4/Node.java
+++ Project2/Part4-6/Question4/Node.java
@@ -4,11 +4,11 @@ class Node //sets what a node is components or vertex in this case
 {
   int data;
   boolean visit;
-  public ArrayList<Node> neighbors;
+  public Hashset<Node> neighbors;
   Node(int data) //the creation of an actual node
   {
     this.data=data;
-    this.neighbors = new ArrayList<>();
+    this.neighbors = new Hashset<>();
     this.visit=false;
   }
 }
